99e098d8486828cfa0a0fa0f736342b0849c5a97
Nikolai Akimov <gaporf@mail.ru>
2022-02-07 12:12:46 +0300

    m

--- PI/uEyeCamera.cpp
+++ PI/uEyeCamera.cpp
@@ -2,77 +2,89 @@
 
 #include "uEyeCamera.h"
 
-uEyeCamera::uEyeCamera() : m_hCam(static_cast<HIDS>(0))
+uEyeCamera::uEyeCamera() : m_hCam(static_cast<HIDS>(0)), m_nBuffersNew(850)
 {
     INT nRet = is_InitCamera(&m_hCam, nullptr);
-    if (nRet == IS_SUCCESS)
+    if (nRet != IS_SUCCESS)
     {
-        nRet = is_ParameterSet(m_hCam, IS_PARAMETERSET_CMD_LOAD_EEPROM, NULL, NULL);
+        throw std::runtime_error("Failed to initiliaze camera");
+    }
+
+    nRet = is_ParameterSet(m_hCam, IS_PARAMETERSET_CMD_LOAD_EEPROM, NULL, NULL);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't load default parameters");
+    }
+
+    nRet = is_GetSensorInfo(m_hCam, &m_sInfo);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't get sensor info");
+    }
+
+    CAMINFO CamInfo;
+    nRet = is_GetCameraInfo(m_hCam, &CamInfo);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't get camera info");
+    }
+
+    m_nSizeX = m_sInfo.nMaxWidth;
+    m_nSizeY = m_sInfo.nMaxHeight;
+    push_log("m_nSizeX = " + std::to_string(m_nSizeX) + ", m_nSizeY = " + std::to_string(m_nSizeY));
+
+    IS_SIZE_2D imageSize;
+    imageSize.s32Width = m_nSizeX;
+    imageSize.s32Height = m_nSizeY;
+
+    if (m_sInfo.nColorMode == IS_COLORMODE_MONOCHROME)
+    {
+        m_nBitsPerPixel = 8;
+        nRet = is_SetColorMode(m_hCam, IS_CM_MONO8);
         if (nRet != 0)
         {
-            push_log("Can't set parameters");
-            return;
+            throw std::runtime_error("Can't set color mode");
         }
-        SENSORINFO SensorInfo;
-        nRet = is_GetSensorInfo(m_hCam, &SensorInfo);
-        if (nRet != 0)
-        {
-            push_log("Can't get sensor info");
-            return;
-        }
-
-        CAMINFO CamInfo;
-        nRet = is_GetCameraInfo(m_hCam, &CamInfo);
-        if (nRet != 0)
-        {
-            push_log("Can't get camera info");
-            return;
-        }
-
-        m_nSizeX = SensorInfo.nMaxWidth;
-        m_nSizeY = SensorInfo.nMaxHeight;
-
-        IS_SIZE_2D imageSize;
-        imageSize.s32Width = m_nSizeX;
-        imageSize.s32Height = m_nSizeY;
-
-        if (SensorInfo.nColorMode == IS_COLORMODE_MONOCHROME)
-        {
-            m_nBitsPerPixel = 8;
-            nRet = is_SetColorMode(m_hCam, IS_CM_MONO8);
-            if (nRet != 0)
-            {
-                push_log("Can't set color mode");
-                return;
-            }
-        }
-        else
-        {
-            push_log("Unknown color mode");
-            return;
-        }
-        m_dwSingleBufferSize = m_nSizeX * m_nSizeY * m_nBitsPerPixel / 8;
-        push_log("Camera type " + std::string(SensorInfo.strSensorName));
-
-        nRet = is_PixelClock(m_hCam, IS_PIXELCLOCK_CMD_GET, reinterpret_cast<void *>(&m_nCamPclk), sizeof(m_nCamPclk));
-        if (nRet != 0)
-        {
-            push_log("Can't get pixel clock");
-            return;
-        }
-        push_log("Pixel clock = " + std::to_string(m_nCamPclk));
-
-        nRet = is_SetFrameRate(m_hCam, IS_GET_FRAMERATE, &newFps);
-        if (nRet != 0)
-        {
-            push_log("Can't get fram rate");
-        }
-        push_log("Frame rate = " + std::to_string(newFps));
     }
     else
     {
-        push_log("Camera failed");
+        throw std::runtime_error("Unknown color mode");
     }
+
+    m_dwSingleBufferSize = m_nSizeX * m_nSizeY * m_nBitsPerPixel / 8;
+    push_log("m_dwSingleBufferSize = " + std::to_string(m_dwSingleBufferSize));
+
+    nRet = is_SetDisplayMode(m_hCam, IS_SET_DM_DIB);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't set display mode");
+    }
+
+    MEMORYSTATUS stat;
+    GlobalMemoryStatus(&stat);
+    m_dwSysMemTotal = stat.dwTotalPhys;
+
+    SeqBuilt();
+    EvInitAll();
+
+    nRet = is_PixelClock(m_hCam, IS_PIXELCLOCK_CMD_GET, (void*)&m_nCamPclk, sizeof(m_nCamPclk));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't get Pixel Clock");
+    }
+
+    double newFps;
+    nRet = is_SetFrameRate(m_hCam, IS_GET_FRAMERATE, &newFps);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't get frame rate");
+    }
+    push_log("fps = " + std::to_string(newFps));
+
+    int nDllVer = is_GetDLLVersion();
+    char strDllVer[32];
+    sprintf(strDllVer, "%d.%02d.%04d", ((nDllVer >> 24) & 0xff), ((nDllVer >> 16) & 0xff), (nDllVer & 0xff));
+    push_log("DLL version = " + std::string(strDllVer));
 }
 
 void uEyeCamera::load_config(std::string path)
@@ -144,4 +156,161 @@ std::string uEyeCamera::get_logs()
         logs.pop();
     }
     return res;
+}
+
+void uEyeCamera::SeqBuilt()
+{
+    unsigned int imax = m_nBuffersNew;
+    IS_SIZE_2D imageSize;
+    INT nRet = is_AOI(m_hCam, IS_AOI_IMAGE_GET_SIZE, (void*)&imageSize, sizeof(imageSize));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("is_AOI returned non-zero value");
+    }
+
+    m_nSizeX = imageSize.s32Width;
+    m_nSizeY = imageSize.s32Height;
+    push_log("m_nSizeX = " + std::to_string(m_nSizeX) + ", m_nSizeY = " + std::to_string(m_nSizeY));
+
+    INT nAllocSizeX = 0;
+    INT nAllocSizeY = 0;
+    UINT nAbsPosX = 0;
+    UINT nAbsPosY = 0;
+
+    nRet = is_AOI(m_hCam, IS_AOI_IMAGE_GET_POS_X_ABS, (void*)&nAbsPosX, sizeof(nAbsPosX));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("is_AOI returned non-zero value");
+    }
+    nRet = is_AOI(m_hCam, IS_AOI_IMAGE_GET_POS_Y_ABS, (void*)&nAbsPosY, sizeof(nAbsPosY));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("is_AOI returned non-zero value");
+    }
+
+    if (nAbsPosX)
+    {
+        nAllocSizeX = m_sInfo.nMaxWidth;
+    }
+    if (nAbsPosY)
+    {
+        nAllocSizeY = m_sInfo.nMaxHeight;
+    }
+
+    IS_RECT rectAOI, rectAOI2;
+    INT NEW_x, NEW_y, NEW_width, NEW_height;
+    nRet = is_AOI(m_hCam, IS_AOI_IMAGE_GET_AOI, (void*)&rectAOI, sizeof(rectAOI));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("is_AOI returned non-zero value");
+    }
+
+    NEW_x = rectAOI.s32X;
+    NEW_y = rectAOI.s32Y;
+    NEW_width = rectAOI.s32Width;
+    NEW_height = rectAOI.s32Height;
+    //rectAOI2.s32X = NEW_x | IS_AOI_IMAGE_POS_ABSOLUTE;
+    //rectAOI2.s32Y = NEW_y | IS_AOI_IMAGE_POS_ABSOLUTE;
+    rectAOI2.s32X = NEW_x;
+    rectAOI2.s32Y = NEW_y;
+    rectAOI2.s32Width = NEW_width;
+    rectAOI2.s32Height = NEW_height;
+    nRet = is_AOI(m_hCam, IS_AOI_IMAGE_SET_AOI, (void*)&rectAOI2, sizeof(rectAOI2));
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("is_AOI returned non-zero value");
+    }
+
+    m_dwSingleBufferSize = NEW_width * NEW_height * m_nBitsPerPixel / 8;
+    push_log("m_dwSingleBufferSize = " + std::to_string(m_dwSingleBufferSize));
+
+    unsigned int i;
+    for (i = 0; i < imax; i++)
+    {
+        nRet = is_AllocImageMem(m_hCam, NEW_width, NEW_height, m_nBitsPerPixel, &m_pcSeqImgMem[i], &m_lSeqMemId[i]);
+        if (nRet != IS_SUCCESS)
+        {
+            throw std::runtime_error("Can't allocate memory");
+        }
+
+        nRet = is_AddToSequence(m_hCam, m_pcSeqImgMem[i], m_lSeqMemId[i]);
+        if (nRet != IS_SUCCESS)
+        {
+            is_FreeImageMem(m_hCam, m_pcSeqImgMem[i], m_lSeqMemId[i]);
+            throw std::runtime_error("AddToSequence returned non-zero value");
+        }
+        m_nSeqNumId[i] = i + 1;
+    }
+
+    m_nBuffersInUse = i;
+    m_nBufferStart = 0;
+    m_nBufferStop = m_nBuffersInUse - 1;
+}
+
+void uEyeCamera::EvInitAll()
+{
+    EvEnumerate();
+
+    for (int i = 0; i < m_EvMax; i++)
+    {
+        m_hEv[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        if (m_hEv[i] == NULL)
+        {
+            throw std::runtime_error("Can't create event");
+        }
+        EvInit(m_nEvUI[i]);
+        EvEnable(m_nEvUI[i], true);
+        m_nEvCount[i] = 0;
+    }
+}
+
+void uEyeCamera::EvEnumerate()
+{
+    m_nEvUI[0] = IS_SET_EVENT_FRAME;
+    m_nEvUI[1] = IS_SET_EVENT_SEQ;
+    m_nEvUI[2] = IS_SET_EVENT_CAPTURE_STATUS;
+    m_nEvUI[3] = IS_SET_EVENT_DEVICE_RECONNECTED;
+    m_nEvUI[4] = IS_SET_EVENT_REMOVE;
+
+    m_EvMax = 5;
+}
+
+void uEyeCamera::EvInit(INT nEvent)
+{
+    int nIndex;
+    EvGetIndex(nEvent, &nIndex);
+    INT nRet = is_InitEvent(m_hCam, m_hEv[nIndex], nEvent);
+    if (nRet != IS_SUCCESS)
+    {
+        throw std::runtime_error("Can't initialize event");
+    }
+}
+
+void uEyeCamera::EvGetIndex(INT nEvent, int *pnIndex)
+{
+    for (int i = 0; i < m_EvMax; i++)
+    {
+        if (m_nEvUI[i] == nEvent)
+        {
+            *pnIndex = i;
+            return;
+        }
+    }
+    throw std::runtime_error("Can't find index");
+}
+
+void uEyeCamera::EvEnable(INT nEvent, bool bEnable)
+{
+    if (bEnable)
+    {
+        INT nRet = is_EnableEvent(m_hCam, nEvent);
+        if (nRet != IS_SUCCESS)
+        {
+            throw std::runtime_error("Can't enable event");
+        }
+    }
+    else
+    {
+        is_DisableEvent(m_hCam, nEvent);
+    }
 }
--- PI/uEyeCamera.h
+++ PI/uEyeCamera.h
@@ -17,16 +17,52 @@ struct uEyeCamera
 
     void capture();
 private:
+
+#define MIN_SEQ_BUFFERS 3
+#define MAX_SEQ_BUFFERS 32767
+
     HIDS m_hCam;
+    SENSORINFO m_sInfo;
     INT m_nSizeX, m_nSizeY;
     INT m_nBitsPerPixel;
     UINT64 m_dwSingleBufferSize;
+    HWND m_hWndDisplay;
+    UINT64 m_dwSysMemTotal;
+    UINT m_nBuffersNew;
+
+    INT		m_lSeqMemId[MAX_SEQ_BUFFERS];
+    char*	m_pcSeqImgMem[MAX_SEQ_BUFFERS];
+    int		m_nSeqNumId[MAX_SEQ_BUFFERS];
+
+    UINT	m_nBuffersInUse;
+    int		m_nBufferStart;
+    int		m_nBufferStop;
+
+#define MAX_EV      32
+
+    HANDLE  m_hEv[MAX_EV];			// event handle array
+    INT     m_nEvUI[MAX_EV];		// type of uEye event
+    int     m_nEvCount[MAX_EV];		// event counter
+    int     m_EvMax;				// maximum number of events we use
+
     int m_nCamPclk;
     double newFps;
 
     std::queue<std::string> logs;
 
     void push_log(std::string log);
+
+    void SeqBuilt();
+
+    void EvInitAll();
+
+    void EvEnumerate();
+
+    void EvInit(INT nEvent);
+
+    void EvGetIndex(INT nEvent, int *pnIndex);
+
+    void EvEnable(INT nEvent, bool bEnable);
 };
 
 #endif // UEYECAMERA_H
